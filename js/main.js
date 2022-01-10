8f09264f18618869a8568d3126b0b38f7e25510c
DanyGlez94 <daniel_glez94@hotmail.com>
2022-01-10 02:18:48 -0600

    Primer Commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const getProfile = async() => {
+
+    try {
+        // const response = await fetch(`https://api.github.com/users/danyglez94`);
+        console.log(response);
+
+        if (response.status === 200) {
+            const data = await response.json();
+            console.log(data);
+            let profilePic = `<img src="${data.avatar_url}">`;
+            // document.getElementById('container').innerHTML = profilePic;
+
+        } else if (response.status === 404){
+            console.log('Usuario no encontrado');
+        } else {
+            console.log('Hubo un error desconocido');
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+getProfile();
