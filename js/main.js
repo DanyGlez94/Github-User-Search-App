ecb118c2963cbc875299ecfb8cf441ee089e63f6
DanyGlez94 <daniel_glez94@hotmail.com>
2022-01-10 20:58:25 -0600

    Terminados los estilos en darkmode para la vista mobile, también mofifiqué los svg, les quité el atributo fill para agregarlo yo con css, los inserté directo en el html

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 const getProfile = async() => {
 
     try {
-        // const response = await fetch(`https://api.github.com/users/danyglez94`);
+        const response = await fetch(`https://api.github.com/users/octocat`);
         console.log(response);
 
         if (response.status === 200) {
             const data = await response.json();
             console.log(data);
             let profilePic = `<img src="${data.avatar_url}">`;
-            // document.getElementById('container').innerHTML = profilePic;
+            document.getElementById('cardImg').innerHTML = profilePic;
 
         } else if (response.status === 404){
             console.log('Usuario no encontrado');
