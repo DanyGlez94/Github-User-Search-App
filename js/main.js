a30c1651f686bc823876e1963f5d69c725898e3e
DanyGlez94 <daniel_glez94@hotmail.com>
2022-01-12 20:44:40 -0600

    Terminado el light mode

--- js/main.js
+++ js/main.js
@@ -33,32 +33,32 @@ const getProfile = async(user) => {
             let location = data.location;
             if (location === null) {
                 document.getElementById('location').innerHTML = 'Not Available';
-                document.getElementById('location').style.opacity = '75%';
-                document.getElementById('locationSVG').style.opacity = '75%';
+                document.getElementById('location').style.opacity = '50%';
+                document.getElementById('locationSVG').style.opacity = '50%';
             } else {
                 document.getElementById('location').innerHTML = location;
             }
             let blog = data.blog;
             if (blog === "") {
                 document.getElementById('blog').innerHTML = 'Not Available';
-                document.getElementById('blog').style.opacity = '75%';
-                document.getElementById('blogSVG').style.opacity = '75%';
+                document.getElementById('blog').style.opacity = '50%';
+                document.getElementById('blogSVG').style.opacity = '50%';
             } else {
                 document.getElementById('blog').innerHTML = `<a target="_blank" href="${blog}">${blog}</a>`;
             }
             let twitter = data.twitter_username;
             if (twitter === null) {
                 document.getElementById('twitter').innerHTML = 'Not Available';
-                document.getElementById('twitter').style.opacity = '75%';
-                document.getElementById('twitterSVG').style.opacity = '75%';
+                document.getElementById('twitter').style.opacity = '50%';
+                document.getElementById('twitterSVG').style.opacity = '50%';
             } else {
                 document.getElementById('twitter').innerHTML = `<a target="_blank" href="https://twitter.com/${twitter}">${twitter}</a>`;
             }
             let company = data.company;
             if (company === null) {
                 document.getElementById('company').innerHTML = 'Not Available';
-                document.getElementById('company').style.opacity = '75%';
-                document.getElementById('companySVG').style.opacity = '75%';
+                document.getElementById('company').style.opacity = '50%';
+                document.getElementById('companySVG').style.opacity = '50%';
             } else {
                 document.getElementById('company').innerHTML = company;
             }
@@ -105,3 +105,9 @@ inputSearch.addEventListener('keyup', (e) => {
 });
 
 getProfile("octocat");
+
+const switchMode = document.getElementById('switch');
+
+switchMode.addEventListener('click', () => {
+    document.body.classList.toggle('light');
+});
